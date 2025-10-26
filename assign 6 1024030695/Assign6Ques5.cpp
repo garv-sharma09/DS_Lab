5cf9f72f6a4c0dca5fdc520603b2bbde9eb80159
garv-sharma09 <gsharma_be24@thapar.edu>
2025-10-26 21:12:21 +0530

    Add files via upload

---
+++ assign 6 1024030695/Assign6Ques5.cpp
@@ -0,0 +1,98 @@
+#include <iostream> 
+using namespace std; 
+ 
+class Node { 
+public: 
+    int data; 
+    Node* next; 
+ 
+    Node(int val) { 
+        data = val; 
+        next = nullptr; 
+    } 
+}; 
+ 
+class LinkedList { 
+public: 
+    Node* head; 
+    LinkedList() { 
+        head = nullptr; 
+    } 
+ 
+    void insertEnd(int val) { 
+        Node* newNode = new Node(val); 
+        if (head == nullptr) { 
+            head = newNode; 
+            return; 
+        } 
+        Node* temp = head; 
+        while (temp->next != nullptr) 
+            temp = temp->next; 
+        temp->next = newNode; 
+    } 
+ 
+    void makeCircular() { 
+        if (head == nullptr) return; 
+        Node* temp = head; 
+        while (temp->next != nullptr) 
+            temp = temp->next; 
+        temp->next = head;  
+    } 
+ 
+    bool isCircular() { 
+        if (head == nullptr) 
+            return false; 
+ 
+        Node* temp = head->next; 
+ 
+        while (temp != nullptr && temp != head) 
+            temp = temp->next; 
+ 
+        return (temp == head); 
+    } 
+ 
+    void display() { 
+        if (head == nullptr) { 
+            cout << "List is empty\n"; 
+            return; 
+        } 
+        Node* temp = head; 
+        int count = 0; 
+        cout << "List: "; 
+        while (temp != nullptr && count < 10) { 
+            cout << temp->data << " -> "; 
+            temp = temp->next; 
+            count++; 
+        } 
+        cout << "...\n"; 
+    } 
+}; 
+ 
+// Main function 
+int main() { 
+    LinkedList list; 
+    int n, val, choice; 
+ 
+    cout << "Enter number of nodes: "; 
+    cin >> n; 
+    cout << "Enter " << n << " values:\n"; 
+    for (int i = 0; i < n; i++) { 
+        cin >> val; 
+        list.insertEnd(val); 
+    } 
+ 
+    cout << "Do you want to make the list circular? (1 = Yes / 0 = No): "; 
+    cin >> choice; 
+    if (choice == 1) 
+        list.makeCircular(); 
+ 
+    cout << endl; 
+    list.display(); 
+ 
+    if (list.isCircular()) 
+        cout << "The linked list is circular.\n"; 
+    else 
+        cout << "The linked list is NOT circular.\n"; 
+ 
+    return 0; 
+}
